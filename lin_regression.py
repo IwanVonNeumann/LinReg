059637e065a99d2b0e75c0a729d5a8b8bb67cac4
IwanVonNeumann <grabageheap@inbox.lv>
2019-02-03 12:27:05 +0200

    equation matrices definitions simplified

--- lin_regression.py
+++ lin_regression.py
@@ -2,18 +2,18 @@ import numpy as np
 
 
 class LinearRegression(object):
-    weights = None
+    def __init__(self):
+        self.coef_ = None
 
     def fit(self, X, y):
         X = self.__prepend_unit_column(X)
-        n, m = X.shape
-        A = np.array([[np.dot(X[:, i], X[:, j]) for j in range(m)] for i in range(m)])
-        b = np.array([[np.dot(X[:, i], y[:, 0]) for i in range(m)]]).transpose()
-        self.weights = np.linalg.inv(A).dot(b)
+        A = X.T.dot(X)
+        b = X.T.dot(y)
+        self.coef_ = np.linalg.inv(A).dot(b)
 
     def predict(self, X):
         X = self.__prepend_unit_column(X)
-        return np.dot(X, self.weights)
+        return np.dot(X, self.coef_)
 
     @staticmethod
     def __prepend_unit_column(X):
