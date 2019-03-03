276d8ae9df1abbe33deb87cbc0d2c5632f3d2a7a
IwanVonNeumann <grabageheap@inbox.lv>
2019-03-03 14:59:58 +0200

    error plot py code refactored

--- error_plot.py
+++ error_plot.py
@@ -5,28 +5,29 @@ from mpl_toolkits.mplot3d import axes3d
 from matplotlib import cm
 
 
-def squares_sum(w1, w2):
-    return w1 ** 2 + w1 * w2 + w2 ** 2
+def squares_sum(w0, w1):
+    return w0 ** 2 + w0 * w1 + w1 ** 2
 
 
-w1 = np.arange(-2, 2, 0.1)
-w2 = np.arange(-2, 2, 0.1)
-w1, w2 = np.meshgrid(w1, w2)
-L = squares_sum(w1, w2)
+w0, w1 = np.meshgrid(
+    np.arange(-2, 2, 0.1),
+    np.arange(-2, 2, 0.1)
+)
 
-L = (L - L.min()) / (L.max() - L.min())  # normalization
+J = squares_sum(w0, w1)
+J = (J - J.min()) / (J.max() - J.min())  # normalization - to match colormap spectre
 
-colors = cm.Spectral(L)
+colors = cm.Spectral(J)
 rcount, ccount, _ = colors.shape
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-surf = ax.plot_surface(w1, w2, L, rcount=rcount, ccount=ccount, facecolors=colors, shade=False)
+surf = ax.plot_surface(w0, w1, J, rcount=rcount, ccount=ccount, facecolors=colors, shade=False)
 surf.set_facecolor((0, 0, 0, 0))
 
 ax.set_xlabel('$w_0$')
 ax.set_ylabel('$w_1$')
-ax.set_zlabel('L')
+ax.set_zlabel('J')
 
 ax.xaxis.set_ticklabels([])
 ax.yaxis.set_ticklabels([])
